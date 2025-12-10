cb16c7ae0f1ff7c2ace82c7c21954e756a482ca3
FIXED-TERM Vinesh S R (BGSW/PJ-ETA-H) <zne1cob@bosch.com>
2025-12-10 15:34:26 +0530

    sidebar reload fix

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -17,6 +17,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import Link from "next/link";
 
 // Menu items.
 export const items = [
@@ -67,10 +68,10 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
